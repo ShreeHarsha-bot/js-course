e2e2fef3aa2bc0491d6f3163996313fd5bdaac1e
Shree Harsha H L <shreeharshahl@gmail.com>
2025-09-03 11:39:52 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const rollNO="UC32HS2301"
+let name="Harsha"
+var email="harsha@google.com"
+city="Mandya" //sometimes without type also js gives output
+let course;
+
+
+// rollNO="jkfdnjd"   not allowed it is constant
+name="Guru"
+email="guru@gmail.com"
+city="Bengaluru"
+//prefers not use var 
+
+console.log(rollNO);
+console.table([name,email,city,course])
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+let age=18
+let isLogin=false
+let state=null
+
+//number => 2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined =>value not assigned
+//symbol =>uniqueness
+
+
+//object
+
+console.log(typeof age)
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
